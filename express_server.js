17d8899e79d70342c96b5f6b9280b6811873f286
Pegasusor001 <linus.ji.001@gmail.com>
2021-06-07 16:23:09 -0700

    ejs added

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs"); // This tells the Express app to use EJS as its templating engine. 
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca", // shortURL is the key, b2xVn2; long is the value. 
   "9sm5xK": "http://www.google.com"
 };
 
@@ -11,8 +13,22 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };   // initate for loop in ejs html file
+  res.render("urls_index", templateVars);       // ejs default folder view, 
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
+  res.render("urls_show", templateVars); // the way to pass the variable to ejs file. 
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
