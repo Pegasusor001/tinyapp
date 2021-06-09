f395dc124ba1a2b9c211f930142797cef85cd3e6
Pegasusor001 <linus.ji.001@gmail.com>
2021-06-09 16:15:45 -0700

    final eidition

--- express_server.js
+++ express_server.js
@@ -34,7 +34,7 @@ app.get("/urls", (req, res) => {
   console.log(req.cookies)
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    userId: req.cookies["userId"]
   };
   res.render("urls", templateVars);     
 });
@@ -43,7 +43,7 @@ app.get("/urls", (req, res) => {
 // Get posting new link page, localhost/urls/news, used to post new urls. 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    userId: req.cookies["userId"]
   };
   res.render("urls_new", templateVars);
 });
@@ -51,7 +51,7 @@ app.get("/urls/new", (req, res) => {
 // Get Register Page
 app.get('/urls/register', (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    userId: req.cookies["userId"]
   };
   res.render('urls_register', templateVars);
 })
@@ -59,7 +59,7 @@ app.get('/urls/register', (req, res) => {
 // Get Login Page
 app.get('/urls/login', (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    userId: req.cookies["userId"]
   };
   res.render('urls_login', templateVars);
 })
@@ -98,23 +98,23 @@ app.post('/urls/:url/edit', (req, res) => {
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  let username = ''
+  let userId = ''
 
   for (let user in userDatabase) {
     if (userDatabase[user].email === email && userDatabase[user].password === password) {
-      username = user;
+      userId = user;
     } else {
-      return res.status(404).send('Please enter the right email or password')
+      return res.status(400).send('Please enter the right email or password')
     }
   }
 
-  res.cookie('username', email)
+  res.cookie('userId', email)
   res.redirect('/urls')
 })
 
 // Post logout
 app.post('/logout', (req, res) => {
-  res.clearCookie('username')
+  res.clearCookie('userId')
   res.redirect('/urls')
 })
 
@@ -123,19 +123,25 @@ app.post('/logout', (req, res) => {
 app.post('/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const username = generateId()
+  const userId = generateId()
 
   if (!email || !password){
-    return res.status(404).send('Please enter username or password')
+    return res.status(404).send('Please enter userId or password')
   }
 
-  userDatabase[username] = {
-    id: username,
+  for (let user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      return res.status(400).send('The email address has been registered')
+    } 
+  }
+
+  userDatabase[userId] = {
+    id: userId,
     email,
     password
   }
 
-  res.cookie('username', email);
+  res.cookie('userId', email);
   res.redirect('/urls');
 })
 
